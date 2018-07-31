eba667d10508875aedad5a8987279a6f5903cd5f
Jimmy Romero <jimmy.romero@fundacion-jala.org>
2018-07-31 16:51:58 -0400

    Task 4

--- quickstart/src/main/java/org/gradle/Person.java
+++ quickstart/src/main/java/org/gradle/Person.java
@@ -4,11 +4,23 @@ import org.apache.commons.collections.list.GrowthList;
 
 public class Person {
     private final String name;
+    private final String lastName;
     private final int age;
+    private final String eyeColor;
+    private final String ci;
+    private final String cellphone;
+    private final String petName;
+    private final String address;
 
-    public Person(String name) {
+    public Person(String name, String lastName, int age, String eyeColor, String ci, String cellphone, String petName, String address) {
         this.name = name;
-        this.age = 10;
+        this.age = age;
+        this.lastName = lastName;
+        this.eyeColor = eyeColor;
+        this.ci = ci;
+        this.cellphone = cellphone;
+        this.petName = petName;
+        this.address = address;
         new GrowthList();
     }
 
@@ -16,7 +28,39 @@ public class Person {
         return name;
     }
 
-     public int getAge() {
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
         return age;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public String getPetname() {
+        return petName;
+    }
+
+    public String getCompleteName() {
+        return name + " " + lastName;
+    }
+
+    public String getAddionalDataOfAperson() {
+        return age + " " + eyeColor + " " + ci + " " + cellphone;
+    }
+
+    public String getAddress() {
+        return address;
     }
 }
--- quickstart/src/test/java/org/gradle/PersonTest.java
+++ quickstart/src/test/java/org/gradle/PersonTest.java
@@ -1,12 +1,66 @@
 package org.gradle;
 
 import org.junit.Test;
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class PersonTest {
+    public Person person;
+
+    @Before
+    public void init() {
+        person = new Person("Jin", "Kazama", 23,
+                "LightBlue", "8001234", "70707070", "Princess", "Mi casa");
+    }
+
     @Test
     public void canConstructAPersonWithAName() {
-        Person person = new Person("Larry");
-        assertEquals("Larry", person.getName());
+        assertEquals("Jin", person.getName());
+    }
+
+    @Test
+    public void canConstructAPersonWithALastName() {
+        assertEquals("Kazama", person.getLastName());
+    }
+
+    @Test
+    public void testGetTheAgeOfAPerson() {
+        assertEquals(23, person.getAge());
+    }
+
+    @Test
+    public void testGetTheEyeColorOfAPerson() {
+        assertEquals("LightBlue", person.getEyeColor());
+    }
+
+    @Test
+    public void testGetCIOfAPerson() {
+        assertEquals("8001234", person.getCi());
+    }
+
+    @Test
+    public void testGetCellphoneOfAPerson() {
+        assertEquals("70707070", person.getCellphone());
+    }
+
+    @Test
+    public void testGetPetNameOfAPerson() {
+        assertEquals("Princess", person.getPetname());
+    }
+
+    @Test
+    public void testGetCompleteNameOfAPerson() {
+        assertEquals("Jin Kazama", person.getCompleteName());
+    }
+
+    @Test
+    public void testGetAdditionalDataOfAPerson() {
+        assertEquals("23 LightBlue 8001234 70707070", person.getAddionalDataOfAperson());
+    }
+
+    @Test
+    public void testGetAddressOfAPerson() {
+        assertEquals("Mi casa", person.getAddress());
     }
 }
