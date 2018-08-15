7da5f6b76deaaffede3fbd578eace6d97d11f941
JimmyRomero <jimmy.romero@fundacion-jala.org>
2018-08-15 19:25:45 -0400

    changes in script

--- webapp-quickstart/deploy-app.sh
+++ webapp-quickstart/deploy-app.sh
@@ -1,11 +1,11 @@
 echo 'Stopping Tomcat service...'
 sudo systemctl stop tomcat
-sudo rm -f "${tomcat_webapps}"/"${war_name}".war, ignoreError: true
-sudo rm -rf "${tomcat_webapps}"/"${war_name}", ignoreError: true
+sudo rm -f /opt/tomcat/webapps/webapp-quickstart.war, ignoreError: true
+sudo rm -rf /opt/tomcat/webapps/webapp-quickstart, ignoreError: true
 
 echo 'Sending war file...'
-sudo cp -f /tmp/"${war_name}".war "${tomcat_webapps}"/"${war_name}".war, ignoreError: true
-sudo rm /tmp/"${war_name}".war
+sudo cp -f /tmp/webapp-quickstart.war /opt/tomcat/webapps/webapp-quickstart.war, ignoreError: true
+sudo rm /tmp/webapp-quickstart.war
 
 echo 'Starting Tomcat service...'
 sudo systemctl start tomcat
