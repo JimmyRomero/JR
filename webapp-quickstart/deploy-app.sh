00b2b2344e510203afc1cc1480cb9788d38bcfa5
JimmyRomero <jimmy.romero@fundacion-jala.org>
2018-08-15 18:08:02 -0400

    test without quotes

--- webapp-quickstart/deploy-app.sh
+++ webapp-quickstart/deploy-app.sh
@@ -1,12 +1,11 @@
 echo 'Stopping Tomcat service...'
 sudo systemctl stop tomcat
-sudo rm -f "${tomcat_webapps}/${war_name}.war", ignoreError: true
-echo '${tomcat_webapps}/${war_name}'
-sudo rm -rf "${tomcat_webapps}/${war_name}", ignoreError: true
+sudo rm -f ${tomcat_webapps}/${war_name}.war, ignoreError: true
+sudo rm -rf ${tomcat_webapps}/${war_name}, ignoreError: true
 
 echo 'Sending war file...'
-sudo cp -f /tmp/"${war_name}.war" "${tomcat_webapps}/${war_name}.war", ignoreError: true
-sudo rm /tmp/"${war_name}.war"
+sudo cp -f /tmp/${war_name}.war ${tomcat_webapps}/${war_name}.war, ignoreError: true
+sudo rm /tmp/${war_name}.war
 
 echo 'Starting Tomcat service...'
 sudo systemctl start tomcat
