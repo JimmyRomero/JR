4c0c2b7484647f0b3e229293f76b7472ff3a0a31
JimmyRomero <jimmy.romero@fundacion-jala.org>
2018-08-15 19:28:44 -0400

    fix in cp line

--- webapp-quickstart/deploy-app.sh
+++ webapp-quickstart/deploy-app.sh
@@ -4,7 +4,7 @@ sudo rm -f /opt/tomcat/webapps/webapp-quickstart.war, ignoreError: true
 sudo rm -rf /opt/tomcat/webapps/webapp-quickstart, ignoreError: true
 
 echo 'Sending war file...'
-sudo cp -f /tmp/webapp-quickstart.war /opt/tomcat/webapps/webapp-quickstart.war, ignoreError: true
+sudo cp -f /tmp/webapp-quickstart.war /opt/tomcat/webapps/webapp-quickstart.war
 sudo rm /tmp/webapp-quickstart.war
 
 echo 'Starting Tomcat service...'
